14af8937bdb383957c31d4667e1709dc00ef022f
BlossomBuild <blossombuildLLC@outlook.com>
2025-10-16 22:29:10 -0700

    Initial Commit

---
+++ BloomBoard/BloomBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomBoardApp.swift
+//  BloomBoard
+//
+//  Created by Carlos Valentin on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct BloomBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
