2d39b40340f0c4e5a9d9bbb1ceed316240e9cdf6
BlossomBuild <blossombuildLLC@outlook.com>
2025-10-16 22:37:02 -0700

    Started post model

---
+++ BloomBoard/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  BloomBoard
+//
+//  Created by Carlos Valentin on 10/15/25.
+//
+
+import Foundation
+
+struct Post {
+    var title: String
+    var image: Data?
+    var creationDate: Date
+}
