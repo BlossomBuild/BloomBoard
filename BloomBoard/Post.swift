d7fce59a214e6f9a5f1e9bc23aa213e02a6acb4d
BlossomBuild <blossombuildLLC@outlook.com>
2025-10-30 00:25:43 -0700

    Completed Post model and added test data

--- BloomBoard/Post.swift
+++ BloomBoard/Post.swift
@@ -7,18 +7,25 @@
 
 import Foundation
 import SwiftData
+import SwiftUI
 
 @Model
 class Post: Identifiable {
-    var id: UUID
+    private(set) var id: UUID
     var title: String
-    var image: Data?
-    var creationDate: Date
+    @Attribute(.externalStorage) var image: Data?
+    private(set) var creationDate: Date
     
-    init(id: UUID = UUID(), title: String, image: Data? = nil, creationDate: Date = Date()) {
-        self.id = id
+    init(title: String, image: Data? = nil) {
+        self.id = UUID()
         self.title = title
         self.image = image
-        self.creationDate = creationDate
+        self.creationDate = Date()
     }
+    
+    static var testPosts = [
+        Post(title: "AI loves to add more code than needed"),
+        Post(title: "Is MVVM needed in SwiftUI?", image: UIImage(named: "Test1")?.jpegData(compressionQuality: 0.90)),
+        Post(title: "How do you write buttons in SwiftUI?", image: UIImage(named: "Test2")?.jpegData(compressionQuality: 0.90))
+    ]
 }
