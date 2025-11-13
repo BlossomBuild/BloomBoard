4cbaaa30078ef1d561d8f6924cc5b56cf8fda622
BlossomBuild <blossombuildLLC@outlook.com>
2025-11-12 23:40:33 -0700

    Completed PostItemView

--- BloomBoard/PostItemView.swift
+++ BloomBoard/PostItemView.swift
@@ -10,17 +10,26 @@ import SwiftUI
 struct PostItemView: View {
     let post: Post
     
+    var hasImage: Bool {
+        post.image != nil
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(post.title)
                 .bold()
                 .font(.title3)
             
-            Text("Created \(post.creationDate, style: .date)")
+            HStack {
+                Text("Created \(post.creationDate, style: .date)")
+                
+                Image(systemName: hasImage ? "photo.artframe" : "document")
+            }
+            .foregroundStyle(.secondary)
         }
     }
 }
 
 #Preview {
-    PostItemView(post: Post.testPosts[0])
+    PostItemView(post: Post.testPosts[1])
 }
